f9486c30566163183ff2704d176e94d020e9ef4f
Rohan Kapoor <rohanneilkapoor@gmail.com>
2015-09-13 13:27:01 -0700

    changed capabilities and equipment page

--- js/skm.js
+++ js/skm.js
@@ -64,4 +64,30 @@ function gradualfade(cur2){
 }
 
 
+(function($){
+// Find the toggles and hide their content
+    $('.toggle').each(function(){
+        $(this).find('.toggle-content').hide();
+    });
+
+    // When a toggle is clicked (activated) show their content
+    $('.toggle a.toggle-trigger').click(function(){
+        var el = $(this), parent = el.closest('.toggle');
+
+        if( el.hasClass('active') )
+        {
+            parent.find('.toggle-content').slideToggle();
+            el.removeClass('active');
+        }
+        else
+        {
+            parent.find('.toggle-content').slideToggle();
+            el.addClass('active');
+        }
+        return false;
+    });
+
+})(jQuery);
+
+
 
